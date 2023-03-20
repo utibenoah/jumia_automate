0ad75a496cdaf82c5a9c9d3c9acad1a164917535
Utibe Noah Effiong <utibenoaheffiong@gmail.com>
2023-03-20 02:54:09 +0100

    add to cart

---
+++ cypress/e2e/add_to_cart.cy.js
@@ -0,0 +1,11 @@
+
+describe('add to cart', () => {
+  it('cart', () => {
+    cy.visit('www.jumia.com.ng')
+    cy.get("a[href='/health-beauty/']").should('be.visible').click({force: true})
+    cy.get(':nth-child(2) > .card > .crs-w > .crs > :nth-child(1) > .prd').click()
+    cy.get('#add-to-cart > .add').should('be.visible').click({force: true})
+    cy.get('a[href="/cart/"]').should('be.visible').click()
+    
+  })
+})
