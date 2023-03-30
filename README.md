c8489034f6b779379e02d40a2c6622154150847a
Utibe Noah Effiong <utibenoaheffiong@gmail.com>
2023-03-30 03:36:59 +0100

    f

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+
+#npm install
+#npx cypress open---- to run the script
 # jumia_automate
 automating jumia wbsite
+#this project automat jumia login, registration and add to cart funtionality
--- cypress/e2e/add_to_cart.cy.js
+++ cypress/e2e/add_to_cart.cy.js
@@ -1,6 +1,6 @@
 
 describe('add to cart', () => {
-  it('cart', () => {
+  it.only('cart', () => {
     cy.visit('www.jumia.com.ng')
     cy.get("a[href='/health-beauty/']").should('be.visible').click({force: true})
     cy.get(':nth-child(2) > .card > .crs-w > .crs > :nth-child(1) > .prd').click()
@@ -8,4 +8,13 @@ describe('add to cart', () => {
     cy.get('a[href="/cart/"]').should('be.visible').click()
     
   })
+
+
+  it('registration', () => {
+    
+  });
+
+  it('login', () => {
+    
+  });
 })
